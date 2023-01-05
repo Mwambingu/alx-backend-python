716ca489b52e7f97534eacc9a51782851fb129d3
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 20:54:49 +0300

    Hot Fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,14 +4,11 @@ Contains a function that takes a float multiplier
 as argument and returns a function that multiplies
 a float by multiplier
 """
-from typing import Callable, Optional
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """function that takes a float multiplier
     as argument and returns a function that multiplies
     a float by multiplier"""
-    def func_mult(n):
-        y = n
-        return y * n
-    return func_mult
+    return lambda x: x * multiplier
