313d475410577839c8437828adf09a4a80437e33
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 18:50:42 +0300

    Task 2

--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -13,3 +13,14 @@ str2 = "shell"
 
 print(concat(str1, str2) == "{}{}".format(str1, str2))
 print(concat.__annotations__)
+
+# Task 2
+import math
+
+floor = __import__('2-floor').floor
+
+ans = floor(3.14)
+
+print(ans == math.floor(3.14))
+print(floor.__annotations__)
+print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains a function that takes a float and
+returns it's floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """function that takes a float and
+    returns it's floor"""
+    return math.floor(n)
