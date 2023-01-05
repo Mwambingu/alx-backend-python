590d3207e59b3867fbabe8ec328c51b3734a770a
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 21:30:03 +0300

    Advanced TaskII

--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -84,3 +84,11 @@ print(element_length.__annotations__)
 safe_first_element =  __import__('100-safe_first_element').safe_first_element
 
 print(safe_first_element.__annotations__)
+
+# Task 11
+safely_get_value = __import__('101-safely_get_value').safely_get_value
+annotations = safely_get_value.__annotations__
+
+print("Here's what the mappings should look like")
+for k, v in annotations.items():
+    print( ("{}: {}".format(k, v)))
