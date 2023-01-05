6719a22d111aa430314ad1f2b74ba59a5688b7ef
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 18:57:32 +0300

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains a function takes a float arg
+and returns it's string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """function takes a float arg
+    and returns it's string representation"""
+    return str(n)
