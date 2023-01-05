ae41eab72a41bf3fa45bc71ae2a8046a454c380f
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 16:54:13 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Contains a function that adds two floats
+and returns a sum of the floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Function that adds two floats"""
+    return a + b
