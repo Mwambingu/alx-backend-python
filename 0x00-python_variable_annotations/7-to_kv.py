863a38234d521349630b440ed6ef1307da5e1789
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-05 20:24:46 +0300

    Hot Fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
-Contains function to_kv that takes a string k and an int OR float v as arguments and returns a tuple
+Contains function to_kv that takes a string k and
+an int OR float v as arguments and returns a tuple
 """
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Function to_kv that takes a string k and an int OR float v as arguments and returns a tuple"""
-    return (k, float(v*v))
+    """Function to_kv that takes a string k and
+    an int OR float v as arguments and returns a tuple"""
+    return (k, float(v * v))
